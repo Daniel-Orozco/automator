c465be78c1e8858193dac3223b7ed72afbc0ab44
Daniel-Orozco <geoluisd@hotmail.com>
2016-02-27 20:07:04 -0800

    First functional prototype, needs operation log and more testing

--- js/panels.js
+++ js/panels.js
@@ -1,8 +1,9 @@
 var txt = '';
 var command = '';
+var result = 0;
 
-var numbers = new Array();
-var operators = new Array();
+var numbers = new Stack();
+var operators = new Stack();
 
 function captureInput(e) {
     var code = (e.keyCode ? e.keyCode : e.which);
@@ -10,21 +11,61 @@ function captureInput(e) {
         command = document.getElementById("input").value;
         characters = command.split('');
 
+
         if (isValid()) {
-            command = '';
+
+            var arithmeticError = false;
+
             for (i = 0; i < characters.length; i++) {
-                if(characters[i] != ' ')
-                    command += characters[i] + "\n";
+                if (characters[i] == ' ') {
+                    continue;
+                }
+                if (characters[i] == '=') {
+                    break;
+                }
+                else {
+                    //Number
+                    if (characters[i] >= '0' && characters[i] <= '9') {
+                        var numword = '';
+                        while (i < characters.length && characters[i] >= '0' && characters[i] <= '9')
+                            numword += characters[i++];
+                        numbers.push(parseInt(numword));
+                    }
+                    //Parenthesis
+                    else if (characters[i] == '(') {
+                        operators.push(characters[i]);
+                    }
+                    else if (characters[i] == ')') {
+                        while (operators.peek() != '(')
+                            numbers.push(applyOperation(operators.pop(), numbers.pop(), numbers.pop()));
+                        operators.pop();
+                    }
+                    //Operations
+                    if (characters[i] == '+' || characters[i] == '-' || characters[i] == '*' || characters[i] == '/') {
+                        while (operators.peek() != null && higherOrder(characters[i], operators.peek()))
+                            numbers.push(applyOperation(operators.pop(), numbers.pop(), numbers.pop()));
+                        operators.push(characters[i]);
+                    }
+                }
             }
 
-            txt += command + "=====\n";
-            document.getElementById("operations").innerHTML = txt;
-
-            document.getElementById('message').innerHTML = "";
+            if (arithmeticError == false) {
+                while (operators.peek() != null) {
+                    numbers.push(applyOperation(operators.pop(), numbers.pop(), numbers.pop()));
+                }
+            }
+            result = numbers.pop();
         }
         else {
             document.getElementById('message').innerHTML = "Error: Invalid expression";
         }
+
+        //
+        txt += result + "=====\n";
+        document.getElementById("operations").innerHTML = txt;
+
+        document.getElementById('message').innerHTML = " ";
+        //
 
         document.getElementById('input').value = "";
         if(event.preventDefault) event.preventDefault();
@@ -67,11 +108,57 @@ function applyOperation(operator, b, a) {
     return 0;
 }
 
-
 function higherOrder(op1, op2) {
     if (op2 == '(' || op2 == ')')
         return false;
     if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
         return false;
     return true;
+}
+
+function Stack(){
+    this.top = null;
+    this.count = 0;
+
+    this.getCount = function(){
+        return this.count;
+    }
+
+    this.getTop = function(){
+        return this.top;
+    }
+
+    this.push = function(data){
+        var node = {
+            data : data,
+            next : null
+        }
+
+        node.next = this.top;
+        this.top = node;
+
+        this.count++;
+    }
+
+    this.peek = function(){
+        if(this.top === null){
+            return null;
+        }else{
+            return this.top.data;
+        }
+    }
+
+    this.pop = function(){
+        if(this.top === null){
+            return null;
+        }else{
+            var out = this.top;
+            this.top = this.top.next;
+            if(this.count>0){
+                this.count--;
+            }
+
+            return out.data;
+        }
+    }
 }
